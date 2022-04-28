4da3d284c838df8d8bbe364c2755f9511e408b2a
Kellbber <kellbberbarcarolo@gmail.com>
2022-04-28 05:42:11 -0300

    feature - adicionando uso de useState

--- src/components/XisLista.jsx
+++ src/components/XisLista.jsx
@@ -1,19 +1,42 @@
-import './XisLista.css';
-import {xis} from "../mocks/xis.js";
-function XisLista (){
-    return <div className="XisLista">
-        {xis.map((xis, index) => (
+import React, { useState } from "react";
+import "./XisLista.css";
+import { xis } from "../mocks/xis.js";
+function XisLista() {
+    const [xisSelecionado, setxisSelecionado] = useState({});
+
+    const adicionarItem = (xisIndex) => {
+            const xis = { [xisIndex]: Number(xisSelecionado[xisIndex] || 0) +1 }
+            setxisSelecionado({ ...xisSelecionado, ...xis});
+    }
+  return (
+    <div className="XisLista">
+      {xis.map((xis, index) => (
         <div className="XisListaItem " key={`XisListaItem-${index}`}>
-            <div>
-                <div className="XisListaItem__titulo">{xis.titulo}</div>
-                <div className="XisListaItem__preco">{xis.preco.toFixed(2)}</div>
-                <div className="XisListaItem__descricao">{xis.descricao}</div>
-                <div className="XisListaItem__acoes Acoes"><button className="Acoes__adicionar Acoes__adicionar--preencher">ADICIONAR</button></div>
+          <span className="XisListaItem__badge">
+            {xisSelecionado[index] || 0}
+          </span>
+          <div>
+            <div className="XisListaItem__titulo">{xis.titulo}</div>
+            <div className="XisListaItem__preco">{xis.preco.toFixed(2)}</div>
+            <div className="XisListaItem__descricao">{xis.descricao}</div>
+            <div className="XisListaItem__acoes Acoes">
+              <button
+                className="Acoes__adicionar Acoes__adicionar--preencher"
+                onClick={() => adicionarItem(index)}
+              >
+                ADICIONAR
+              </button>
             </div>
-            <img  className="XisListaItem__foto" src={xis.foto}alt={`Xis de ${xis.sabor}`}/>
+          </div>
+          <img
+            className="XisListaItem__foto"
+            src={xis.foto}
+            alt={`Xis de ${xis.sabor}`}
+          />
         </div>
-        ))}
-         </div>
+      ))}
+    </div>
+  );
 }
 
 export default XisLista;
